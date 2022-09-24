6980dfebe66f6e39b949bd8be0c06952e82047c8
juneidea <juneidea@yahoo.com>
2022-09-23 22:04:54 -0500

    border style

--- src/Graph.js
+++ src/Graph.js
@@ -5,14 +5,14 @@ import { data, scale } from './data'
 
 function Graph({selected}) {
     const sortData = data.sort((a, b) => (a[selected] > b[selected]) ? -1 : 1)
-    const ratio = scale[selected].ratio *0.6*0.8
+    const ratio = scale[selected].ratio
     return (
       <div className="Graph">
         {sortData.map(person => {
             return (
-            <div>
+            <div className="Graph-border">
                 <span className="Graph-name">{person.name}</span>
-                <div className="Graph-border"><div className="Bar" style={{width: `${person[selected]*ratio}vw`}}></div></div>
+                <div className="Graph-bar"><div className="Bar" style={{width: `${person[selected]*ratio}%`}}></div></div>
             </div>)
         })}
       </div>
